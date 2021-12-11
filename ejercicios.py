f47acdd04ae25aa5fab38d220425092cab300a15
FELPIL07 <emperadorfelipe01@gmail.com>
2021-12-11 15:58:38 -0500

    metodo por opcion

--- ejercicios.py
+++ ejercicios.py
@@ -27,6 +27,104 @@ from decimal import Decimal
 def raizCuadrada(a):
      return a
 
+string("¿Qué opción deseas elegir? ** ESCOGE CON EL NÚMERO **")
+i = 0
+
+# print("suma: 1")
+# print("resta: 2")
+# print("multiplicación: 3")
+# print("división: 4")
+# print("potencia cuadrada: 5")
+# print("potencia cubica: 6 \n")
+
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+
+print("1( suma )")
+print("2( resta )")
+print("3( multiplicacion )")
+print("4( division )")
+print("5( potencia )")
+print("6( potencia cubica )")
+
+
+numero = (input("Elige tu opción: "))
+
+if numero == a or "suma":
+     string("** SUMA **")
+     i = 0
+     while True:
+          try:
+               a = int(input("Escribe un número: "))
+               b = int(input("Escribe un número: "))
+               print("La suma de {} y {} es: {}".format(a,b,suma(a,b)))
+          except:
+               print("Por favor escribe un número entero")
+          i += 1
+
+elif numero == b or "resta":
+     string("** RESTA **")
+     i = 0
+     while True:
+          try:
+               a = int(input("Escribe un número: "))
+               b = int(input("Escribe un número: "))
+               print("La resta de {} y {} es: {}".format(a,b,resta(a,b)))
+          except:
+               print("Por favor escribe un número entero")
+          i += 1
+     
+elif numero == 3:
+     string("** MULTIPLICACIÓN **")
+     i = 0
+     while True:
+          try:
+               a = int(input("Escribe un número: "))
+               b = int(input("Escribe un número: "))
+               print("La multiplicación de {} y {} es: {}".format(a,b,multiplicacion(a,b)))
+          except:
+               print("Por favor escribe un número entero")
+          i += 1
+
+elif numero == 4:
+     string("** DIVISIÓN **")
+     i = 0
+     while True:
+          try:
+               a = int(input("Escribe un número: "))
+               b = int(input("Escribe un número: "))
+               print("La división de {} y {} es: {}".format(a,b,division(a,b)))
+          except:
+               print("Por favor escribe un número entero")
+          i += 1
+
+elif numero == 5:
+     string("** POTENCIA CUADRADA **")
+     i = 0
+     while True:
+          try:
+               a = int(input("Escribe un número: "))
+               print("La potencia cuadrada de {} es: {}".format(a,potencia(a)))
+          except:
+               print("Por favor escribe un número entero")
+          i += 1
+     
+elif numero == 6:
+     string("** POTENCIA CUBICA **")
+     i = 0
+     while True:
+          try:
+               a = int(input("Escribe un número: "))
+               print("La potencia cubica de {} es: {}".format(a,potenciaCubo(a)))
+          except:
+               print("Por favor escribe un número entero")
+          i += 1
+
+    
 # string("** SUMA **")
 # i = 0
 # end = 100
